334c370d756dd9683d96d27af38a2208147bef2a
kamhar-ngado <kamhar.ngado.id@gmail.com>
2022-04-17 05:06:56 +0800

    add months

---
+++ style.js
@@ -0,0 +1,19 @@
+const d = new Date();
+const months = [
+  "Januari",
+  "Februari",
+  "Maret",
+  "April",
+  "Mei",
+  "Juni",
+  "Juli",
+  "Agustus",
+  "September",
+  "Oktober",
+  "November",
+  "Desember",
+];
+
+let month = months[d.getMonth()];
+document.getElementById("tanggal").innerHTML = month;
+
